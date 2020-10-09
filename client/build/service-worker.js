6b5d608fe9bce6b5a34cc41178ba927a2ebddb9c
novaprecious <60961422+novaprecious@users.noreply.github.com>
2020-10-09 09:56:02 +0100

    make it better

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d140d9cdfe56d42fece8ba84a4aa7663.js"
+  "/precache-manifest.129969bd5e5da058962769e5491ea66a.js"
 );
 
 self.addEventListener('message', (event) => {

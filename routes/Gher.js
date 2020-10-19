27bb34446a10981661cab1dd217a41c2c6a70eb2
novaprecious <60961422+novaprecious@users.noreply.github.com>
2020-10-19 14:44:58 +0100

    make it better

--- routes/Gher.js
+++ routes/Gher.js
@@ -6,75 +6,8 @@ const OneDayGher = require("../models/1DGher");
 const FourDayGher = require("../models/4DGher");
 const SevenDayGher = require("../models/7DGher");
 const PendingGher = require("../models/pendingGher");
-const User = require("../models/user");
-const { celebrate, Joi, Segments } = require("celebrate");
 const verifyAdmin = require("../verifyAdmin");
 
-router.post(
-  "/:id",
-  celebrate({
-    [Segments.PARAMS]: Joi.object().keys({
-      id: Joi.string().required(),
-    }),
-    [Segments.BODY]: Joi.object().keys({
-      amount: Joi.number().integer().required(),
-    }),
-  }),
-  async (req, res) => {
-    try {
-      const regGher = await User.findByIdAndUpdate(
-        req.params.id,
-        {
-          wantToCashout: true,
-        },
-        { new: true, runValidators: true, context: "query" }
-      );
-      const newGher = new Gher({
-        username: regGher.username,
-        email: regGher.email,
-        amount: req.body.amount,
-      });
-      const savedGher = await newGher.save();
-      res.json(savedGher);
-      console.log("Gher saved");
-    } catch (err) {
-      res.json({ message: err });
-    }
-  }
-);
-
-router.delete(
-  "/:id",
-  celebrate({
-    [Segments.PARAMS]: Joi.object().keys({
-      id: Joi.string().required(),
-    }),
-  }),
-  async (req, res) => {
-    try {
-      const reversedGher = await User.findByIdAndUpdate(
-        req.params.id,
-        {
-          wantToCashout: false,
-        },
-        { new: true, runValidators: true, context: "query" }
-      );
-      const deletedGher = await Gher.findByIdAndDelete(req.params.id);
-      res.json("Gher is deleted");
-    } catch (err) {
-      res.json({ message: err });
-    }
-  }
-);
-
-router.get("/all-details", async (req, res) => {
-  try {
-    const foundGher = await Gher.find();
-    res.json(foundGher);
-  } catch (err) {
-    res.json({ message: err });
-  }
-});
 router.get("/", async (req, res) => {
   try {
     const foundGher = await Gher.find({}, "email amount");
@@ -121,34 +54,6 @@ router.get("/pending", verifyAdmin, async (req, res) => {
     res.json(foundPendingGher);
   } catch (err) {
     res.json({ message: err });
-  }
-});
-router.get("/:email", async (req, res) => {
-  try {
-    const oneDayGher = await OneDayGher.findOne(
-      { email: req.params.email },
-      "amount"
-    );
-    const fourDayGher = await FourDayGher.findOne(
-      { email: req.params.email },
-      "amount"
-    );
-    const sevenDayGher = await SevenDayGher.findOne(
-      { email: req.params.email },
-      "amount"
-    );
-    const verify = (v) => {
-      if (v === null) return 0;
-      return v.amount;
-    };
-    const totalAmt =
-      verify(oneDayGher) + verify(fourDayGher) + verify(sevenDayGher);
-    if (totalAmt == 0) return res.status(200).send({ ghStatus: {} });
-    res
-      .status(200)
-      .json({ ghStatus: { email: req.params.email, amount: totalAmt } });
-  } catch (err) {
-    res.status(400).send(err.message);
   }
 });
 
--- routes/Pher.js
+++ routes/Pher.js
@@ -1,75 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Pher = require("../models/pher");
-const User = require("../models/user");
-const { celebrate, Joi, Segments } = require("celebrate");
 const verifyAdmin = require("../verifyAdmin");
 
-router.post(
-  "/:id",
-  celebrate({
-    [Segments.PARAMS]: Joi.object().keys({
-      id: Joi.string().required(),
-    }),
-    [Segments.BODY]: Joi.object().keys({
-      amount: Joi.number().integer().required(),
-    }),
-  }),
-  async (req, res) => {
-    try {
-      const regPher = await User.findByIdAndUpdate(
-        req.params.id,
-        {
-          wantToInvest: true,
-        },
-        { new: true, runValidators: true, context: "query" }
-      );
-      const newPher = new Pher({
-        username: regPher.username,
-        email: regPher.email,
-        amount: req.body.amount,
-      });
-      const savedPher = await newPher.save();
-      res.json(savedPher);
-      console.log("Pher saved");
-    } catch (err) {
-      res.json({ message: err });
-    }
-  }
-);
-
-router.delete(
-  "/:id",
-  celebrate({
-    [Segments.PARAMS]: Joi.object().keys({
-      id: Joi.string().required(),
-    }),
-  }),
-  async (req, res) => {
-    try {
-      const reversedPher = await User.findByIdAndUpdate(
-        req.params.id,
-        {
-          wantToCashout: false,
-        },
-        { new: true, runValidators: true, context: "query" }
-      );
-      const deletedPher = await Pher.findByIdAndDelete(req.params.id);
-      res.json("user is deleted");
-    } catch (err) {
-      res.json({ message: err });
-    }
-  }
-);
-
-router.get("/all-detail", async (req, res) => {
-  try {
-    const foundPher = await Pher.find();
-    res.json(foundPher);
-  } catch (err) {
-    res.json({ message: err });
-  }
-});
 router.get("/", verifyAdmin, async (req, res) => {
   try {
     const foundPher = await Pher.find({}, "email amount");
